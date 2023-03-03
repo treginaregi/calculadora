7600724c667b674c6a1054c72d046c0bce8541a0
treginaregi <a01654087@tec.mx>
2023-03-03 11:19:57 -0600

    Operaciones con fracciones

--- calculadora/urls.py
+++ calculadora/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('procesamiento', views.procesamiento, name = 'procesamiento'),
+    path('suma', views.suma, name = 'suma'),
+    path('resta', views.resta, name = 'resta'),
+    path('multiplicacion', views.multiplicacion, name = 'multiplicacion'),
+    path('division', views.division, name = 'division'),
 ]
--- calculadora/views.py
+++ calculadora/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from json import loads,dumps
 
 # Create your views here.
 def nueva():
@@ -16,5 +18,69 @@ def procesamiento(request):
     #vas a invocar una página html que se llama "proceso"
     #le vas a pasar una variable que se llama "name" cuyo contenido es un nombre
     #desde el punto de vista de Python es un diccionario
+
+@csrf_exempt
+def suma(request):
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+    num1 = body['num1']
+    den1 = body['den1']
+    num2 = body['num2']
+    den2 = body['den2']
+    if den1 == den2:
+        num_resultado = num1 + num2
+        den_resultado = den1
+    else:
+        den_resultado = den1 * den2
+        num_resultado = int(((den_resultado/den1)*num1) - ((den_resultado/den2)*num2))
+    resultado = Fraccion(num_resultado,den_resultado)
+    json_resultado = resultado.toJSON()
+    return HttpResponse(json_resultado, content_type = "text/json-comment-filtered")
+
+@csrf_exempt
+def resta(request):
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+    num1 = body['num1']
+    den1 = body['den1']
+    num2 = body['num2']
+    den2 = body['den2']
+    if den1 == den2:
+        num_resultado = num1 - num2
+        den_resultado = den1
+    else:
+        den_resultado = den1 * den2
+        num_resultado = int(((den_resultado/den1)*num1) - ((den_resultado/den2)*num2))
+    resultado = Fraccion(num_resultado,den_resultado)
+    json_resultado = resultado.toJSON()
+    return HttpResponse(json_resultado, content_type = "text/json-comment-filtered")
+
+@csrf_exempt
+def multiplicacion(request):
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+    num1 = body['num1']
+    den1 = body['den1']
+    num2 = body['num2']
+    den2 = body['den2']
+    num_resultado = num1 * num2
+    den_resultado = den1 * den2
+    resultado = Fraccion(num_resultado,den_resultado)
+    resultado_json = resultado.toJSON()
+    return HttpResponse(resultado_json, content_type = "text/json-comment-filtered")
+
+@csrf_exempt
+def division(request):
+    body_unicode = request.body.decode('utf-8')
+    body = loads(body_unicode)
+    num1 = body['num1']
+    den1 = body['den1']
+    num2 = body['num2']
+    den2 = body['den2']
+    num_resultado = num1 * den2
+    den_resultado = den1 * num2
+    resultado = Fraccion(num_resultado,den_resultado)
+    resultado_json = resultado.toJSON()
+    return HttpResponse(resultado_json, content_type = "text/json-comment-filtered")
 
 
